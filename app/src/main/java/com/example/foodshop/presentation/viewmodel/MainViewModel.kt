77354eb393bcd5a786a3bd1bd98af4e910189cc6
Artem <adpm@list.ru>
2023-06-17 15:48:17 +0600

    Добавлен тестовый вывод данных

--- app/src/main/java/com/example/foodshop/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/foodshop/presentation/viewmodel/MainViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.foodshop.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodshop.domain.usecases.GetAllFoodData
+import kotlinx.coroutines.launch
 
 class MainViewModel(
-
+ private val getAllFoodData: GetAllFoodData
 ) : ViewModel(){
 
+    init{
+        viewModelScope.launch {
+            Log.d("VM", getAllFoodData().toString())
+        }
+    }
 }
